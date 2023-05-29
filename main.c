37f8b88f1c7182e2f0d83b494c41af1a7695f4b7
leandro <leandro.campoliete@live.com>
2023-05-29 00:56:20 -0300

    Replanteando

--- main.c
+++ main.c
@@ -1,8 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+
+void gotoxy (int x, int y);
+void pantInic ();
+
 
 int main()
 {
-    printf("Hello world!\n");
+    pantInic();
+
+
+
+
+
+
+
+
+//Sacamos el mensaje final
+    getch();
     return 0;
+}
+
+
+
+
+
+
+
+void gotoxy (int x, int y)
+{
+    //Controlador de ventana y manipulador de consola
+
+    HANDLE ventana;
+    ventana=GetStdHandle(STD_OUTPUT_HANDLE);
+
+    //Creamos objeto de las coordenadas dentro de la estructura
+    COORD coordenadas;
+
+    coordenadas.X= x;
+    coordenadas.Y=y;
+
+
+    SetConsoleCursorPosition(ventana,coordenadas);
+}
+
+void pantInic ()
+{
+    int x;
+    int y;
+    int vida=0;
+    int contador;
+
+    gotoxy(40,10);
+    printf("Welcome to Game of the Life\n");
+
+    for(x=20; x<90; x++)
+    {
+        for(y=20; y<60; y++)
+        {
+            gotoxy(x,y);
+            printf("%c");
+        }
+    }
+
+    gotoxy(34,21);
+    printf("%c",219);
+    gotoxy(35,21);
+    printf("%c",219);
+    gotoxy(36,21);
+    printf("%c", 219);
+    gotoxy(36,20);
+    printf("%c",219);
+    gotoxy(35,19);
+    printf("%c",219);
+
+
+
+
+
+    if (vida==1&&contador<3)
+    {
+        gotoxy(x,y);
+        printf("%c", 219);
+
+    }
+
+
+
+
+
 }
